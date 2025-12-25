e07490383812dc473149894dfe030483d9b99e8d
Sandeep Ramgolam <sandeep@ramgolam.com>
2025-12-25 21:44:58 +0400

    improve site footer

--- src/components/site-footer.vue
+++ src/components/site-footer.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="flex flex-col justify-center gap-4 text-center text-sm text-white md:flex-row md:justify-between">
-    <router-link :to="{ name: 'statistics' }"> <carbon-chart-cluster-bar />Statistics </router-link>
-    <a href="https://github.com/MrSunshyne/mauritius-dataset-electricity"> <carbon-data-base />Data Source </a>
-
-    <a href="https://sandeep.ramgolam.com"> <carbon-paint-brush />Made by Sandeep Ramgolam </a>
-    <a href="https://github.com/MrSunshyne/mauritius-power-outages"> <carbon-logo-github />Source Code </a>
+  <div class="border-t border-white/10 pt-6">
+    <div class="grid grid-cols-2 gap-4 text-sm text-white/70 sm:flex sm:flex-wrap sm:justify-center sm:gap-x-8 sm:gap-y-3">
+      <router-link :to="{ name: 'statistics' }" class="link">
+        <carbon-chart-cluster-bar />
+        <span>Statistics</span>
+      </router-link>
+      <a href="https://github.com/MrSunshyne/mauritius-dataset-electricity" class="link">
+        <carbon-data-base />
+        <span>Data Source</span>
+      </a>
+      <a href="https://sandeep.ramgolam.com" class="link">
+        <carbon-paint-brush />
+        <span>Made by Sandeep</span>
+      </a>
+      <a href="https://github.com/MrSunshyne/mauritius-power-outages" class="link">
+        <carbon-logo-github />
+        <span>Source Code</span>
+      </a>
+    </div>
   </div>
 </template>
 
 <style scoped>
-a {
-  @apply hover:underline;
+.link {
+  @apply flex items-center gap-2 text-white/60 hover:text-white transition-colors;
 }
 
-a svg {
-  @apply mr-2 inline-block;
+.link svg {
+  @apply w-4 h-4 flex-shrink-0;
 }
 </style>
