aaebc35863c630afb2e263d27c171c82f6e0fee4
Sandeep Ramgolam <sandeep@ramgolam.com>
2025-12-27 00:59:00 +0400

    Implement Outage Selection and handle previous denavigation.

--- components/CellGroup.vue
+++ components/CellGroup.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid gap-4 sm:gap-6">
     <template v-for="outage in props.data" :key="outage.id">
-      <Cell :data="outage" />
+      <Cell :data="outage" :selected-outage-id="props.selectedOutageId" />
     </template>
   </div>
 </template>
@@ -11,5 +11,6 @@ import type { Record } from '~/types'
 
 const props = defineProps<{
   data?: Record[]
+  selectedOutageId?: string | null
 }>()
 </script>
